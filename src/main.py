d6731ffd1a05182ad415095f57acafde43b642f8
Ariel <ariel.kerry@gmail.com>
2023-02-16 16:11:44 -0500

    finish color-coding placeable nodes

--- src/main.py
+++ src/main.py
@@ -6,10 +6,15 @@ from ui_utils import *
 from logic import * 
 from UI_CONSTANTS import * 
 
+# TODO: some sort of confirmation sent over nt that the robot has successfully placed the item
+
 def main():
 	pygame.init()
 	NetworkTables.initialize()
+
 	table = NetworkTables.getTable('SmartDashboard') # Maybe change the table?
+	# TODO actually test this w/ nt
+	# ll_object_data = table.getAutoUpdateValue("llpython") 
 
 	pygame.display.set_caption('Place Cones/Cubes Interface')
 	WIDTH = SQUARE_SIDE_LENGTH * 9
@@ -27,10 +32,10 @@ def main():
 				is_running = False
 			elif event.type == pygame.MOUSEBUTTONDOWN:
 				grid_coords = get_grid_coord_from_click(event.pos)
-				table.putNumber("Target_Grid_Coord", [float(x) for x in grid_coords])
+				table.putNumberArray("Target_Grid_Coord", [float(x) for x in grid_coords])
+				print([float(x) for x in grid_coords])
 			else:
-				# TODO: color code placeable grid nodes
-				draw_grid(window_surface)
+				draw_grid(window_surface, 1)
 				pygame.display.update()
 
 main()
--- src/ui_utils.py
+++ src/ui_utils.py
@@ -16,13 +16,15 @@ import pygame
 from UI_CONSTANTS import * 
 from COLORS import * 
 
-def get_color_from_num(n):
-	if n == 0:
+def get_color_from_num(n, object):
+	if n == 0 and object == 0:
 		return CUBE
-	elif n == 1:
+	elif n == 1 and object == 1:
 		return CONE
-	elif n == 2:
+	elif n == 2 and not object == -1:
 		return HYBRID
+	else:
+		return INACTIVE
 
 grid = [
 	[1, 0, 1] * 3,
@@ -30,13 +32,13 @@ grid = [
 	[2, 2, 2] * 3,
 ]
 
-def draw_grid(surface):
+def draw_grid(surface, object):
 	global grid
 	x = 0
 	y = 0
 	for i in range(len(grid)):
 		for j in range(len(grid[i])):
-			color = get_color_from_num(grid[i][j])
+			color = get_color_from_num(grid[i][j], object)
 			pygame.draw.rect(surface, color, pygame.Rect(x, y, SQUARE_SIDE_LENGTH, SQUARE_SIDE_LENGTH))
 			x += SQUARE_SIDE_LENGTH + GRID_GAP
 		y += SQUARE_SIDE_LENGTH + GRID_GAP
